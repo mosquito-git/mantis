280a6d54a93165800e28312570e7d22f4fdb6012
Evgeny thinkpad <evg_lenovo@example.com>
2022-06-21 23:58:45 +0300

    pop3

--- fixture/application.py
+++ fixture/application.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
 from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
 
 
 
@@ -26,6 +28,8 @@ class Application:
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
         self.james = JamesHelper(self)
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
         self.config = config
         self.base_url = config['web']['baseUrl']
 
